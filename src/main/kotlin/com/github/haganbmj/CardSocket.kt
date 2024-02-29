a3cf37b625aaa1877de49e636c94e250b95949d1
Brendan Hagan <bhaganksu@gmail.com>
2024-02-29 00:10:54 -0500

    misc: socket logging

--- src/main/kotlin/com/github/haganbmj/CardSocket.kt
+++ src/main/kotlin/com/github/haganbmj/CardSocket.kt
@@ -1,5 +1,6 @@
 package com.github.haganbmj
 
+import io.quarkus.logging.Log
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.websocket.OnClose
 import jakarta.websocket.OnError
@@ -16,16 +17,19 @@ class CardSocket(
 
     @OnOpen
     fun open(session: Session, @PathParam("id") id: String) {
+        Log.info("open@$id: ${session.id}")
         cardSessions.open(session, id)
     }
 
     @OnClose
     fun close(session: Session, @PathParam("id") id: String) {
+        Log.info("close@$id: ${session.id}")
         cardSessions.close(session, id)
     }
 
     @OnError
     fun error(session: Session, @PathParam("id") id: String, throwable: Throwable) {
+        Log.error("error@$id: ${session.id}", throwable)
         cardSessions.close(session, id)
     }
 }
